162442f85d701a92a3bb8a775e4bac8c2ff04043
davisclrk <dclark304@gatech.edu>
2024-04-06 14:32:04 -0400

    solved top k freq elems

---
+++ TopKFrequentElements.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <map>
+
+using namespace std;
+
+int main(){
+    vector<int> nums;
+    int k;
+    map<int,int> mp;
+    for (int i:nums) mp[i]++;
+    vector<pair<int,int>> v;
+    for (auto &i:mp){
+        v.push_back({i.second, i.first});
+    }
+    sort(v.begin(),v.end());
+    reverse(v.begin(),v.end());
+    vector<int> ans;
+    for (int i=0;i<k;i++){
+        ans.push_back(v[i].second);
+    }
+    // return ans;
+}
