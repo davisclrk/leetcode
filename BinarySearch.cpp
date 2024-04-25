56a2e410fe5317b257bc524babb3f93c159d0fe9
davisclrk <dclark304@gatech.edu>
2024-04-25 17:50:55 -0400

    solved binary search

---
+++ BinarySearch.cpp
@@ -0,0 +1,20 @@
+#include <vector>
+
+using namespace std;
+
+int main(){
+    vector<int> nums;
+    int target;
+
+    int l = 0, r = nums.size()-1;
+    while (l <= r){
+        int mid = l + (r - l)/2; // equivalent to "(l + r) / 2" but is done in this order to prevent overflow
+        if (nums[mid] == target) return mid;
+        else if (nums[mid] > target) r = mid - 1;
+        else l = mid + 1;
+    }
+    return -1;
+
+    // basic binary search template code
+    // returns -1 if element is not found and loop is broken
+}
